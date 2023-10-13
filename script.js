439767f6df4c0b268001251c67b83dbbd5e19f6e
kanekii16 <96989943+kanekii16@users.noreply.github.com>
2023-10-13 13:21:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+	const element = document.getElementById("colorSelect");
+
+
+const input = document.getElementsByTagName("input")[0];
+
+input.addEventListener("click",()=>{
+    element.remove(element.value);
+})
+
+	
